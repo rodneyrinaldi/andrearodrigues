b0715167e8056ef86e55eabb1461828c0693155e
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2022-03-21 14:40:01 -0300

    feat: add landing page moema

--- src/pages/lps/nosm/index.js
+++ src/pages/lps/nosm/index.js
@@ -4,7 +4,15 @@ import React from "react";
 function Page() {
   const router = useRouter();
 
-  return <></>;
+  React.useEffect(() => {
+    router.push("/lps/nosm/landing");
+  }, []);
+
+  return (
+    <>
+      <p>carregando...</p>
+    </>
+  );
 }
 
 export default Page;
